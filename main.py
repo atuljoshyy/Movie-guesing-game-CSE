c1dc89f718a7012f60405ac504ab1fa7df887346
Atul Joshy <80053501+atuljoshyy@users.noreply.github.com>
2021-12-15 00:03:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,72 @@
 import random
 
-movie_list = ["Dune","Eternals","Venom","spider-man no way home","No time to die","Black panther","Free guy","Jungle cruise","Old","Cruella","Nobody","The suicde squad","The resident evil","Dolittle","The batman","Tenet","The tomorrow war","Mortal kombat","Cinderella","Men in black","Wonder woman 1984","Oblivion","Red notice","The lion king","Blood shot"]
+print('''
+
+█▀▄▀█ █▀█ █░█ █ █▀▀   █▀▀ █░█ █▀▀ █▀ █▀ █ █▄░█ █▀▀   █▀▀ ▄▀█ █▀▄▀█ █▀▀
+█░▀░█ █▄█ ▀▄▀ █ ██▄   █▄█ █▄█ ██▄ ▄█ ▄█ █ █░▀█ █▄█   █▄█ █▀█ █░▀░█ ██▄
+
+''')
+
+Movies=["Dune","Eternals","Venom","spider-man no way home","No time to die","Black panther","Free guy","Jungle cruise","Old","Cruella","Nobody","The suicde squad","The resident evil","Dolittle","The batman","Tenet","The tomorrow war","Mortal kombat","Cinderella","Men in black","Wonder woman","Oblivion","Red notice","The lion king","Blood shot"]
 
 
-movie_chosen = random.choice(movie_list).lower()
-used = []
+movie_chosen=random.choice(Movies).lower()
+ch=''
+for i in movie_chosen:
+    if i.isalpha():
+        ch+='_'
+    else:
+        ch+=' '
+ch=movie_chosen[0]+ch[1:-1]+movie_chosen[-1]
 
+print(" ".join(ch))
+print()
 
-display = movie_chosen
-for i in range (len(display)):
+#This function is used if the user wants to guess the entire movie name
+def direct(movie_chosen):
+    user_guess=input('Enter movie name : ')
+    if movie_chosen==user_guess.lower():
+        print('yay!, You got it right 🎉')
+    else:
+        print('Sorry, you got it wrong 😢')
 
-  display = display[0:i]  + "_" + display[i+1:]
+#This function is used if the user wants to guess the movie name letter by letter
+def letters(ch):
+    while movie_chosen!= ch:
+        user_guess=input('Enter your guess : ').lower()
+        if user_guess in movie_chosen:
+            for i in range(len(movie_chosen)):
+                if movie_chosen[i]==user_guess:
+                    ch=ch[0:i]+movie_chosen[i]+ch[i+1:]
+            print(' '.join(ch))
+            print('You got it right ,Keep going! 🔥')
+        else:
+            print(f"'{user_guess}' is not in the movie name 😢")
 
-  
+#This is the main function
+def main():
+    option='y'
+    while option=='y':
+        movie_chosen=random.choice(Movies)
+        print('1. Guess the entire movie name')
+        print('2. Guess the movie name letter by letter')
+        print('3. Exit')
+        print()
+        e=int(input('Choose your option : '))
+        print()
+        if e==1:
+            direct(movie_chosen)
+        elif e==2:
+            letters(ch)
+        elif e==3:
+            print('Have a nice day! 😄')
+            break
+        else:
+            print('Invalid choice ❌')
+        option=input('again? 🤔 (y/n) : ')
+main()
 
+            
+        
+    
 
-movie_display = " ".join(display)
-
-print (movie_chosen[0]+movie_display[1:-1]+movie_chosen[-1])
-
-
-attempts = 0
-
-
-while display != movie_chosen:
-  guess = input("Please enter a letter: ")
-  guess = guess.lower()
-  used.extend(guess)
-  print ("Attempts: ")
-  print (attempts)
-  
-
-  for i in range(len(movie_chosen)):
-    if movie_chosen[i] == guess:
-      display = display[0:i] + guess + display[i+1:]
-
-  print("Used letters: ")
-  print(used)
-      
-
-
-  movie_display = " ".join(display)
-  print (movie_chosen[0]+movie_display[1:-1]+movie_chosen[-1])
-  attempts = attempts + 1
-  
-print("Well done, you guessed the correct movie " + str(attempts) + " attempts")
